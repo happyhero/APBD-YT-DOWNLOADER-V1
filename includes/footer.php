0c16db42fc20085cdc61b2aae74e75080f93a286
happyhero <207630@qq.com>
2018-07-21 21:18:34 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,15 @@
 	</div> <!-- section-padding end -->
+	<div>
+
+
+
+
+
+    © 2018 <a href="/">	youtube-convert.net</a>
+    - Free YouTube Online Media Conversion and Download - <a href="/terms/">Terms of Use</a> - <a href="/terms/#privacy">Privacy</a> 
+	
+
+</div>
 	<script src="assets/js/jquery-1.12.4.min.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
 	<script src="assets/js/main.js"></script>
