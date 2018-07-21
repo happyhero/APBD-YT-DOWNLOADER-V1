72a603d2d52c3bd7e18a78ccf711cf6e732bfab8
happyhero <207630@qq.com>
2018-07-21 21:04:19 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>APBD Youtube Downloader</title>
+	<title>YouTube Converter &amp; Downloader - Convert to MP3, MP4, AVI</title>
+	<meta name="description" content="YouTube Downloader lets you save and convert videos from YouTube and other sites and play them on your computer for free! Available for Windows &amp; Mac
+YouTube to MP3, MP4 Downloader and Converter. HD, 1080p and 4K are supported. Free &amp; fast! No software download needed.ouTube converter, YouTube downloader, YouTube to mp4, YouTube video downloader, YouTube video converter, download YouTube videos, download YouTube.YouTube to MP3, MP4 Downloader and Converter. HD, 1080p and 4K are supported. Free &amp; fast! No software download needed.">
+
+
+
+<meta name="keywords" content="youtube download, freeware, youtube downloader, youtube video download, youtube videos download, download video youtube, download videos youtube, myspace video download, download video from youtube, download videos from youtube, convert video youtube, convert videos youtubes, download and convert video youtube, download and convert videos youtube, how to download video, how to download videos, how to download video from youtube, how to download videos from youtube,YouTube converter, YouTube downloader, YouTube to mp4, YouTube video downloader, YouTube video converter, download YouTube videos, download YouTube,YouTube converter, YouTube downloader, YouTube to mp4, YouTube video downloader, YouTube video converter, download YouTube videos, download YouTube">
+
 	<!-- stylesheet -->
 	<link rel="stylesheet" tpe="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" tpe="text/css" href="assets/css/font-awesome.min.css">
